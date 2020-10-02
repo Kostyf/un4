b144a9800ec49d30488da6ab5167453f3c8ffe9b
Konstantin <kostimedev@gmail.com>
2020-10-02 22:41:00 +0300

    Initial commit

---
+++ src/un4/TestMatch.java
@@ -0,0 +1,8 @@
+package un4;
+
+public class TestMatch {
+    public static void main(String[] args) {
+        Match mat = new Match("Match Football");
+        mat.setLocationRelativeTo(null); // displays the window in the middle of the screen.
+    }
+}
